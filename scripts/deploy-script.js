2ea873b2992641712b2e59d31401af2067e8e1c2
Đức Lê <letrongduc1999@gmail.com>
2021-08-14 23:18:20 +0700

    add-readme

--- scripts/deploy-script.js
+++ scripts/deploy-script.js
@@ -3,19 +3,26 @@
 //
 // When running the script with `npx hardhat run <script>` you’ll find the Hardhat
 // Runtime Environment’s members available in the global scope.
+
+const web3 = require("web3")
+
 const hre = require("hardhat");
 async function main() {
   const [deployer] = await ethers.getSigners();
+  const amount = 1000000000;
+  const totalSupply = web3.utils.toWei(amount.toString(), 'ether')
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
+  const ERC20 = await ethers.getContractFactory("./contracts/contracts.sol:ERC20");
+  const erc20 = await ERC20.deploy("NGL", "NGL", totalSupply);
   const NFT = await ethers.getContractFactory("NFT");
-  const nft = await NFT.deploy(deployer.address);
+  const nft = await NFT.deploy(erc20.address);
   const Buffer = await ethers.getContractFactory("buffer");
   const buffer = await Buffer.deploy();
   const Market = await ethers.getContractFactory("NFTmarketplace");
-  const market = await Market.deploy(deployer.address);
+  const market = await Market.deploy(erc20.address);
   const LockContract = await ethers.getContractFactory("lockContract");
-  const lockContract = await LockContract.deploy(deployer.address);
+  const lockContract = await LockContract.deploy(erc20.address);
   console.log("NFT deployed to:", nft.address);
   console.log("Buffer deployed to:", buffer.address);
   console.log("Market deployed to:", market.address);
