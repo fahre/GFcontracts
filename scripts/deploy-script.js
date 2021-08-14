47eedee41cc1b3051f7c6c482c0943d9875a80db
Đức Lê <letrongduc1999@gmail.com>
2021-08-14 15:43:46 +0700

    deploy-script

--- scripts/deploy-script.js
+++ scripts/deploy-script.js
@@ -1,37 +1,26 @@
 // We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
+// When running the script with `npx hardhat run <script>` you’ll find the Hardhat
+// Runtime Environment’s members available in the global scope.
 const hre = require("hardhat");
-
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
-  // We get the contract to deploy
-  const Buffer = await hre.ethers.getContractFactory("buffer");
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+  const NFT = await ethers.getContractFactory("NFT");
+  const nft = await NFT.deploy(deployer.address);
+  const Buffer = await ethers.getContractFactory("buffer");
   const buffer = await Buffer.deploy();
-
-  await buffer.deployed();
-
-  const IBuffer = await hre.ethers.getContractAt("Ibuffer");
-  const Ibuffer = await IBuffer.deploy();
-
-  await buffer.deployed();
-  await Ibuffer.deployed();
-
+  const Market = await ethers.getContractFactory("NFTmarketplace");
+  const market = await Market.deploy(deployer.address);
+  const LockContract = await ethers.getContractFactory("lockContract");
+  const lockContract = await LockContract.deploy(deployer.address);
+  console.log("NFT deployed to:", nft.address);
   console.log("Buffer deployed to:", buffer.address);
-  console.log("Ibuffer deployed to:", Ibuffer.address);
-
+  console.log("Market deployed to:", market.address);
+  console.log("LockContract deployed to:", lockContract.address);
 }
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
